05c062ed020ad71d9f85e9b5136876bf1e7ae847
patheath <patheath@gmail.com>
2020-04-24 08:16:39 -0400

    base pages built

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './containers/Welcome';
+import Session from './containers/Session';
 
 
 function App() {
 
+  function displayNavLink(name, to) {
+    return (
+      <Link
+        to={to}
+        data-rb-event-key={to}
+        className="nav-link"
+      >
+        {name}
+      </Link>
+    )
+  }
+
   return (
-    <Welcome />
+    <Router>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="/">Planning Poker</Navbar.Brand>
+        <Nav className="mr-auto">
+          {displayNavLink("Welcome", "/")}
+          {displayNavLink("About", "/about")}
+          {displayNavLink("Session", "/session")}
+        </Nav>
+      </Navbar>
+      <Switch>
+        <Route path="/about">
+          <h1>About- Placeholder</h1>
+        </Route>
+        <Route path="/session">
+          <Session />
+        </Route>
+        <Route path="/">
+          <Welcome />
+        </Route>
+      </Switch>
+    </Router >
   );
 }
 
