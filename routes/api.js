d452cb4ac86725119bb1b0f6e8fc559f964368af
patheath <patheath@gmail.com>
2020-06-18 09:07:07 -0400

    cleaning up

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var start = require('../handlers/start.js');
+var putItem = require('../handlers/foo.js')
 
 var myLogger = function (req, res, next) {
   console.log(Date(Date.now()).toString());
@@ -8,6 +10,19 @@ var myLogger = function (req, res, next) {
 }
 
 router.use(myLogger);
+
+/* POST Start a new session, with all emails */
+router.post('/session/start', function(req, res, next) {
+  // pass along to handler
+  const { emails } = req.body; 
+  session = start(emails);
+  res.send(session);
+});
+
+router.post('/session/foo', function(req, res, next) {
+  putItem(req, (result) => ( res.send(result)));
+  console.log("Finished");
+})
 
 /* POST the session */
 router.post('/session/:sessionId', function(req, res, next) {
